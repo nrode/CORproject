3b2f3a0c377cb73d6e10af7db14d7ad99e6529a3
Nicolas O. Rode <nicolas.rode@inrae.fr>
2020-11-05 15:49:02 +0100

    :dog: add data

--- _devhistory.R
+++ _devhistory.R
@@ -1,3 +1,10 @@
+## Add .Git at the root of the project
+usethis::use_git()
+usethis::use_github()
+
+## Create project in Github and follow the instructions
+## Invite collaborators using the Settings directory in Github
+## They can create a project on their computer using version control
 
 ## Buildignore/Gitignore
 usethis::use_build_ignore("_devhistory.R")
@@ -8,3 +15,9 @@ usethis::use_git_ignore(".DS_Store")
 usethis::use_readme_rmd()
 
 usethis::use_mit_license("Nicolas Rode")
+
+## Add a data directory
+dir.create("data")
+
+
+
