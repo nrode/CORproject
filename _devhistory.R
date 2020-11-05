59bc7b78f95119ae08271cf47640ca0e86b4da5d
Nicolas O. Rode <nicolas.rode@inrae.fr>
2020-11-05 15:37:19 +0100

    Initial commit

---
+++ _devhistory.R
@@ -0,0 +1,10 @@
+
+## Buildignore/Gitignore
+usethis::use_build_ignore("_devhistory.R")
+usethis::use_build_ignore(".DS_Store")
+usethis::use_git_ignore(".DS_Store")
+
+## Create README
+usethis::use_readme_rmd()
+
+usethis::use_mit_license("Nicolas Rode")
