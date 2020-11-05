b6b178fdc0067c25d4f97e74de7ca834fb231987
Nicolas O. Rode <nicolas.rode@inrae.fr>
2020-11-05 16:07:44 +0100

    :cow: add dataset

--- _devhistory.R
+++ _devhistory.R
@@ -10,6 +10,8 @@ usethis::use_github()
 usethis::use_build_ignore("_devhistory.R")
 usethis::use_build_ignore(".DS_Store")
 usethis::use_git_ignore(".DS_Store")
+usethis::use_build_ignore("data/.DS_Store")
+usethis::use_git_ignore("data/.DS_Store")
 
 ## Create README
 usethis::use_readme_rmd()
@@ -19,5 +21,19 @@ usethis::use_mit_license("Nicolas Rode")
 ## Add a data directory
 dir.create("data")
 
+usethis::use_r("")
 
+data <- read.csv("./data/FitnessExperimentalDesign_G60Final.csv", head=TRUE, sep=";")
 
+head(data)
+
+library(tidyverse)
+## Clean names
+names(data) <- janitor::make_clean_names(names(data))
+
+data_temp <- data %>%
+  mutate(nbadrep1=rpois(n=nrow(data), lambda = 20))
+
+data_temp
+## Export dataset
+write_csv(data_temp, file= "data/fitness.csv")
