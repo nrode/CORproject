26a6c7a1d003c61e8b3c37a4f652121e5ee504c2
Nicolas O. Rode <nicolas.rode@inrae.fr>
2020-11-05 20:06:38 +0100

    improve the code and build a function that return a vector whose elements are the rows of the initial png

--- R/toto.R
+++ R/toto.R
@@ -1,55 +1,57 @@
+## Function that takes a png and return a vector with the rows found by tesseract
+ocrpng <- function(impagepath="data/test.png"){ ##Path to the png image that will be the argument of the toto function
+
+  ## Test with English training data
+  test <- ocr(image = impagepath, engine = tesseract("eng"))
+  cat(test)
+
+  ## Test with French training data
+  if(is.na(match("fra", tesseract_info()$available))) tesseract_download("fra")
+  test <- ocr(image = impagepath, engine = tesseract("fra"))
+  cat(test)
+
+  ## Clean image with magick
+  library(magick)
+  input <- image_read(impagepath)
+  image_browse(input)
+
+  input %>%
+    image_resize("2000x") %>%     ## Clean the image
+    image_convert(type = 'Grayscale') %>%
+    image_trim(fuzz = 40) %>%
+    image_write(format = 'png', density = '300x300') %>%
+    tesseract::ocr(engine = tesseract(language="fra")) %>%
+    cat()
+
+  text <- input %>%
+    image_resize("2000x") %>% ## Clean the image
+    image_convert(type = 'Grayscale') %>%
+    image_trim(fuzz = 40) %>%
+    image_write(format = 'png', density = '300x300') %>%
+    tesseract::ocr(
+      engine = tesseract(language="fra",
+      options = list(tessedit_char_whitelist = paste(c(" .0123456789", letters, toupper(letters)), collapse=""))))## Specify which number and letters are allowed
+  cat(text)
 
 
-## Test with English training data
-test <- ocr(image="data/test.png", engine = tesseract("eng"))
-cat(test)
+  vec <- unlist(strsplit(text, split="\n", fixed = FALSE, perl = FALSE, useBytes = FALSE))
+  namecol <- vec[1]
 
-## Test with French training data
-if(is.na(match("fra", tesseract_info()$available))) tesseract_download("fra")
-test <- ocr(image="data/test.png", engine = tesseract("fra"))
-cat(test)
+  vec <- vec[-c(1,2)]
 
-## Clean image with magick
-library(magick)
-input <- image_read("data/test.png")
-image_browse(input)
+  ## Replace double space by a single space
+  vec <- gsub(pattern="  ", replacement = " ", vec)
 
-input %>%
-  image_resize("2000x") %>%     ## Clean the image
-  image_convert(type = 'Grayscale') %>%
-  image_trim(fuzz = 40) %>%
-  image_write(format = 'png', density = '300x300') %>%
-  tesseract::ocr(engine = tesseract(language="fra")) %>%
-  cat()
+  vec2 <- strsplit(vec, split=" ", fixed = FALSE, perl = FALSE, useBytes = FALSE)
 
-text <- input %>%
-  image_resize("2000x") %>% ## Clean the image
-  image_convert(type = 'Grayscale') %>%
-  image_trim(fuzz = 40) %>%
-  image_write(format = 'png', density = '300x300') %>%
-  tesseract::ocr(
-    engine = tesseract(language="fra",
-    options = list(tessedit_char_whitelist = paste(c(" .0123456789", letters, toupper(letters)), collapse=""))))## Specify which number and letters are allowed
-cat(text)
+  head(vec2)
 
+  nbcol <- lapply(vec2, length)
 
-vec <- unlist(strsplit(text, split="\n", fixed = FALSE, perl = FALSE, useBytes = FALSE))
-namecol <- vec[1]
+  badrows <- which(nbcol>7)
 
-vec <- vec[-c(1,2)]
+  vec_clean <- vec[!nbcol>7]
+  vec_notclean <- vec[nbcol>7]
+  return(vec_clean)
 
-## Replace double space by a single space
-vec <- gsub(pattern="  ", replacement = " ", vec)
-
-vec2 <- strsplit(vec, split=" ", fixed = FALSE, perl = FALSE, useBytes = FALSE)
-
-head(vec2)
-
-nbcol <- lapply(vec2, length)
-
-badrows <- which(nbcol>7)
-
-vec_clean <- vec[!nbcol>7]
-vec_notclean <- vec[nbcol>7]
-
-
+}
