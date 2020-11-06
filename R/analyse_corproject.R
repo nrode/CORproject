9f6d4bd842e12abbbff9b8a51133149b7fc8406c
camille_imbert <camille.imbert@inrae.fr>
2020-11-06 10:22:25 +0100

    Changement analyse stat : - encore des erreurs dans les fonctions - Rmarkdown actualisé - get ignore actualisé

--- R/analyse_corproject.R
+++ R/analyse_corproject.R
@@ -8,31 +8,36 @@ data_temp<-read.table("FitnessExperimentalDesign_G60Final.csv", header=TRUE, sep
 
 # Graphe par Env
 
-graphe<- function() {
+graphe<- function(data) {
 
-moy_pop_env<-plyr::ddply(data_temp, c("Pop", "Env"), summarise,
+moy_pop_env<-plyr::ddply(data, c("Pop", "Env"), summarise,
                  Moy=mean(Col))
 moy_pop_env
 
-g_pop_env<-ggplot2::ggplot(data=moy_pop_env, aes(x=Pop, y=Moy))+
-  geom_bar(stat="identity", color="black", fill="grey")+
+g_pop_env<-ggplot2::ggplot(data=moy_pop_env, aes(x=Pop, y=Moy, fill=Env))+
+  geom_bar(stat="identity", color="black")+
   theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
   facet_wrap(vars(Env))
 g_pop_env
 
 }
 
+graphe(data_temp)
+
 # Analyse
 
-analyse_stat<- function(){
+analyse_stat<- function(data_temp){
 glmer_mouches<-lme4::glmer(Col~Env+Pop+(1|Block),data=data_temp,family="poisson")
 glmer_mouches
 simulationOutput <- DHARMa::simulateResiduals(fittedModel = glmer_mouches, n = 250) # QQplot
 DHARMa::plotSimulatedResiduals(simulationOutput = simulationOutput, quantreg = TRUE)
-anova_mouches<-lme4::Anova(glmer_mouches, test="Chisq")
+anova_mouches<-car::Anova(glmer_mouches, test="Chisq")
 anova_mouches
 sump<-summary(multcomp::glht(glmer_mouches, mcp("Pop"="Tukey")))
 sump
 sume<-summary(glht(glmer_mouches, mcp("Env"="Tukey")))
 sume
 }
+
+
