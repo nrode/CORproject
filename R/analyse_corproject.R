479c49e0c0668db22afe79d488379d400bc0bf2c
Nicolas O. Rode <nicolas.rode@inrae.fr>
2020-11-06 12:27:31 +0100

    change doc from Camille

--- R/analyse_corproject.R
+++ R/analyse_corproject.R
@@ -1,38 +1,43 @@
 # Fonctions analyse stat COR Project
 
-setwd("C:/Users/Camille/Documents/Essai/CORproject/data")
-
 # Db usage du sol
-data_temp<-read.table("FitnessExperimentalDesign_G60Final.csv", header=TRUE, sep=";", na.strings="NA")
+
 
 
 # Graphe par Env
 
-graphe<- function() {
+graphe<- function(csvpath="./data/fitness.csv") {
 
-moy_pop_env<-plyr::ddply(data_temp, c("Pop", "Env"), summarise,
-                 Moy=mean(Col))
+  data<-read.table(csvpath, header=TRUE, sep=",", na.strings="NA")
+
+moy_pop_env<-plyr::ddply(data, c("pop", "env"), summarise,
+                 Moy=mean(col))
 moy_pop_env
 
-g_pop_env<-ggplot2::ggplot(data=moy_pop_env, aes(x=Pop, y=Moy))+
-  geom_bar(stat="identity", color="black", fill="grey")+
+g_pop_env<-ggplot2::ggplot(data=moy_pop_env, aes(x=pop, y=Moy, fill=env))+
+  geom_bar(stat="identity", color="black")+
   theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
   facet_wrap(vars(Env))
 g_pop_env
 
 }
 
+
 # Analyse
 
-analyse_stat<- function(){
-glmer_mouches<-lme4::glmer(Col~Env+Pop+(1|Block),data=data_temp,family="poisson")
+analyse_stat<- function(data){
+glmer_mouches<-lme4::glmer(Col~Env+Pop+(1|Block),data=data,family="poisson")
 glmer_mouches
 simulationOutput <- DHARMa::simulateResiduals(fittedModel = glmer_mouches, n = 250) # QQplot
-DHARMa::plotSimulatedResiduals(simulationOutput = simulationOutput, quantreg = TRUE)
-anova_mouches<-lme4::Anova(glmer_mouches, test="Chisq")
+plot<-DHARMa::plotSimulatedResiduals(simulationOutput = simulationOutput, quantreg = TRUE)
+anova_mouches<-car::Anova(glmer_mouches, test="Chisq")
 anova_mouches
 sump<-summary(multcomp::glht(glmer_mouches, mcp("Pop"="Tukey")))
 sump
 sume<-summary(glht(glmer_mouches, mcp("Env"="Tukey")))
 sume
+return(plot, anova_mouches, sump,sume)
 }
+
+
